df746f7f5b72e07c6f3ef97f931d00c101faba20
shivi8302 <shivi07k@gmail.com>
2025-12-01 19:49:25 +0530

    burring

---
+++ Image Filtering & Blurring/gaussian_blur.py
@@ -0,0 +1,9 @@
+import cv2
+
+image = cv2.imread('C:\\Users\\shive\\Downloads\\Bin1.jpg')
+
+blurred_image = cv2.GaussianBlur(image, (7,7), 0)
+
+cv2.imshow('Blurred Image', blurred_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
